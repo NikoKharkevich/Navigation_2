ca1cbf05ca716e0c53178328e4923a24f3c7d2a8
Nikolai Kharkevich <niko.kharkevych@gmail.com>
2021-08-09 08:05:32 +0300

    added iOSIntPackage and applied filtres to ModelPost

---
+++ Navigation/Supporting Files/ModelPost.swift
@@ -0,0 +1,12 @@
+import iOSIntPackage
+
+public struct ModelPost {
+
+    let author: String
+    let desciption: String
+    let image: String
+    let likes: Int
+    let views: Int
+    let filter: ColorFilter
+
+}
