f97d356b27d69de3a2d4b83338cabcce10240e9f
Nikolai Kharkevich <niko.kharkevych@gmail.com>
2021-09-06 19:02:52 +0300

    Checker Singleton updated with private init() {}

--- Navigation/Login/Checker.swift
+++ Navigation/Login/Checker.swift
@@ -4,6 +4,10 @@ class Checker {
     private let login = "Dave"
     private let pswd = "pass"
     
+    static var shared = Checker()
+    
+    private init() { }
+    
     func loginCheck(enteredLogin: String, enteredPswd: String) -> Bool {
         if enteredLogin == login && enteredPswd == pswd {
             return true
