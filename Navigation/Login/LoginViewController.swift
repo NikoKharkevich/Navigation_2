51d0291dabd7da0216b7e1700224707773702bde
Nikolai Kharkevich <niko.kharkevych@gmail.com>
2021-09-12 19:28:10 +0300

    fixed login by adding login-pass check to tapOnLoginButton()

--- Navigation/Login/LoginViewController.swift
+++ Navigation/Login/LoginViewController.swift
@@ -118,12 +118,12 @@ class LoginViewController: UIViewController {
 
         #else
         if let enteredNamed = emailField.text,
-           (currentUser.userService(userName: enteredNamed) != nil) {
+           delegate?.loginInspectorCheck(enteredLogin: emailField.text ?? "", enteredPswd: passField.text ?? "") == true  {
             let profileVC = ProfileViewController(userService: currentUser, enteredUserName: enteredNamed)
             navigationController?.pushViewController(profileVC, animated: true)
-            print("Correct login")
+            print("Correct!")
         } else {
-            print("Wrong login")
+            print("Wrong login or password")
         }
 
         #endif
